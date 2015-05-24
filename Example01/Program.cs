4128a7b054a97653ef78ae6215ae5786aea31346
ichen0129 <ichen.lo@gmail.com>
2015-05-24 16:23:21 +0800

    2015-0522

---
+++ Example01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Vector[] vectors = new Vector[5];
+            vectors[0] = new Vector(3, 4);
+            vectors[1] = new Vector(7, 24);
+            vectors[2] = new Vector(5, 12);
+            vectors[3] = new Vector(2, 1);
+            vectors[4] = new Vector(3, 8);
+            Console.WriteLine("排序前");
+            for (int index = 0; index < vectors.Length; index++)
+            {
+                Console.WriteLine(vectors[index].ToString());
+            }
+
+            Console.WriteLine("排序後");
+            Array.Sort(vectors);
+            for (int index = 0; index < vectors.Length; index++)
+            {
+                Console.WriteLine(vectors[index].ToString());
+            }
+        }
+    }
+}
---
+++ Example01/Vector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Vector : IComparable
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double Length
+        {
+            get { return Math.Sqrt(X * X + Y * Y); }
+        }
+
+        public Vector(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Vector B = obj as Vector;
+            return (int)(this.Length - B.Length);
+        }
+
+        public override string ToString()
+        {
+            string s = string.Format("X={0}, Y={1}, Length={2}", this.X, this.Y, this.Length);
+            return s;
+        }
+    }
+}
