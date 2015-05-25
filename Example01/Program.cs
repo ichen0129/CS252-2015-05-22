fcaab8f66c1554df184492b5f1b27b8d1ecb2b55
ichen0129 <ichen.lo@gmail.com>
2015-05-25 16:01:34 +0800

    上課練習

---
+++ Example01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Vector[] vectors = new Vector[5];
+            vectors[0] = new Vector(8, 4);
+            vectors[1] = new Vector(7, 24);
+            vectors[2] = new Vector(5, 12);
+            vectors[3] = new Vector(2, 1);
+            vectors[4] = new Vector(3, 8);
+
+            Console.Write("請指定排序的順序，(1)小到大，(2)大到小:");
+            int OrderOption = int.Parse(Console.ReadLine());
+            Vector.Order = OrderOption == 1 ? 1 : -1;
+            //for (int index = 0; index < vectors.Length; index++)
+            // {
+            //     vectors[index].Order = OrderOption == 1 ? 1 : -1;
+            //}
+
+            Console.WriteLine("排序前");
+            for (int index = 0; index < vectors.Length; index++)
+            {
+                Console.WriteLine(vectors[index]);
+            }
+
+            Console.WriteLine("排序後");
+            Array.Sort(vectors);
+            for (int index = 0; index < vectors.Length; index++)
+            {
+                Console.WriteLine(vectors[index]);
+            }
+        }
+    }
+}
---
+++ Example01/Vector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Vector : IComparable
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        // public int Order { get; set; }
+        public static int Order { get; set; }
+
+        public double Length
+        {
+            get { return Math.Sqrt(X * X + Y * Y); }
+        }
+
+        public Vector(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Vector B = obj as Vector;
+            if (this.Length > B.Length)
+                return 1 * Order;
+            else if (this.Length == B.Length)
+                return 0;
+            else
+                return -1 * Order;
+
+            //return (int)(this.Length - B.Length);
+        }
+
+        public override string ToString()
+        {
+            string s = string.Format("X={0}, Y={1}, Length={2}", this.X, this.Y, this.Length);
+            return s;
+        }
+    }
+}
