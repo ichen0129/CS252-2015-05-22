17b45ec1f431dda32012984827205228e42a8f84
ichen0129 <ichen.lo@gmail.com>
2015-05-25 17:01:16 +0800

    依科目排序

--- Example01/Program.cs
+++ Example01/Program.cs
@@ -19,15 +19,19 @@ namespace Example01
             vectors[4] = new Vector(3, 8);
             */
             Student[] students = new Student[5];
-            students[0] = new Student(80, 95);
-            students[1] = new Student(52, 24);
-            students[2] = new Student(90, 78);
-            students[3] = new Student(66, 75);
-            students[4] = new Student(65, 99);
+            students[0] = new Student(80, 95, 60);
+            students[1] = new Student(52, 80, 77);
+            students[2] = new Student(90, 78, 62);
+            students[3] = new Student(66, 75, 74);
+            students[4] = new Student(65, 99, 66);
+
+            Console.Write("請指定排序科目，(1)國文 (2)英文 (3)數學 (4)總分");
+            int SubjectOption = int.Parse(Console.ReadLine());
 
             Console.Write("請指定排序的順序，(1)小到大，(2)大到小:");
             int OrderOption = int.Parse(Console.ReadLine());
 
+            Student.排序科目 = SubjectOption;
             Student.Order = OrderOption == 1 ? 1 : -1;
             Console.WriteLine("排序前");
             for (int index = 0; index < students.Length; index++)
